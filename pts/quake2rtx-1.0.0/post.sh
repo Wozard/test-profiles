48700eda1358912c535c7bf7047a503fbd8bbcb9
Gary Dunn <gdunn@nvidia.com>
2021-05-21 13:48:41 -0700

    Handle resolution restore with the Phoronix variables instead of xdpyinfo and temp files

--- pts/quake2rtx-1.0.0/post.sh
+++ pts/quake2rtx-1.0.0/post.sh
@@ -2,5 +2,4 @@
 
 # Restore old resolution
 #
-xrandr --size $( cat /tmp/old_resolution )
-rm /tmp/old_resolution
+xrandr --size ${VIDEO_WIDTH}x${VIDEO_HEIGHT}
--- pts/quake2rtx-1.0.0/pre.sh
+++ pts/quake2rtx-1.0.0/pre.sh
@@ -5,7 +5,6 @@ WIDTH=$1
 HEIGHT=$2
 
 # Set resolution
-echo -n "$( xdpyinfo | grep -oP 'dimensions:\s+\K\S+' )" > /tmp/old_resolution
 xrandr --size ${WIDTH}x${HEIGHT}
 
 # Remove old logs
